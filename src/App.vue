e04fa2f9f6d6bd06dd37ca4bb1fc085c1ca82c07
Blessing Krofegha <bkrofegha@gmail.com>
2021-06-23 22:19:24 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 
-    Add the SliceZone to your template, and pass the Slice
+    Add the SliceZone to template, and pass the Slice
     data and a resolver function as props.
   -->
   <SliceZone
@@ -12,8 +12,8 @@
 <script>
 // Import the SliceZone
 import SliceZone from 'vue-slicezone'
-// Import your Slices (ensure the path is correct)
-import TextSlice from './components/slices/TextSlice'
+// Import Slices
+import TextSlice from './slices/TextSlice'
 
 export default {
   components: {
@@ -22,7 +22,7 @@ export default {
   data () {
     return {
       document: null,
-      // Add your Slices to a "slices" object, which
+      // Add Slices to a "slices" object, which
       // will be used in the resolver function.
       slices: {
         TextSlice,
@@ -32,11 +32,12 @@ export default {
   methods: {
     // Define a method to query Prismic for your document.
     getContent: async () => {
-      this.document = await this.$prismic.client.getByUID('page', 'homepage')
+      this.document = await this.$prismic.client.getSingle('page')
+      console.log('RESPONSEEEEE-', this.document.data.body)
     }
   },
   created () {
-    // Call your query method when the component loads.
+    // Call query method when the component loads.
     this.getContent();
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+// `~/src/main.js`
+
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import PrismicVue from '@prismicio/vue'
+import linkResolver from './link-resolver' // Update this path if necessary
 
-Vue.config.productionTip = false
+const accessToken = '' // Leave empty if your repo is public
+const endpoint = 'https://about-page.cdn.prismic.io/api/v2' // Use your repo name
 
+// Register plugin
+Vue.use(PrismicVue, {
+  endpoint,
+  apiOptions: { accessToken },
+  linkResolver
+})
+
+// Create a Vue instance
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App)
 }).$mount('#app')
